d3b759c2541389196ec7fba47a9f866363973ba3
MorozovAP <andre15@inbox.ru>
2019-11-28 16:39:24 +0400

    наравдение стрельбы + фиксы

--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -1,8 +1,11 @@
+#define _USE_MATH_DEFINES
+
 #include "MyStrategy.hpp"
 #include "MathHelper.h"
 #include "Helper.h"
 #include <utility>
 #include <algorithm>
+#include <math.h>
 
 using namespace std;
 
@@ -36,7 +39,9 @@ Vec2Double getBulletCrossBorderPoint(const Bullet& bullet, double maxX, double m
 			return vertCross;
 		}
 
-		return bullet.velocity.y < 0 ? MathHelper::getLinesCross(x1, y1, x2, y2, 0, 0, maxX, 0) : MathHelper::getLinesCross(x1, y1, x2, y2, 0, maxY, maxX, maxY);
+		return bullet.velocity.y < 0 ?
+			MathHelper::getLinesCross(x1, y1, x2, y2, 0, 0, maxX, 0) : 
+			MathHelper::getLinesCross(x1, y1, x2, y2, 0, maxY, maxX, maxY);
 	}
 	else {
 		const auto vertCross = MathHelper::getLinesCross(x1, y1, x2, y2, 0, 0, 0, maxY);
@@ -44,11 +49,61 @@ Vec2Double getBulletCrossBorderPoint(const Bullet& bullet, double maxX, double m
 			return vertCross;
 		}
 
-		return bullet.velocity.y < 0 ? MathHelper::getLinesCross(x1, y1, x2, y2, 0, 0, maxX, 0) : MathHelper::getLinesCross(x1, y1, x2, y2, 0, maxY, maxX, maxY);
+		return bullet.velocity.y < 0 ? 
+			MathHelper::getLinesCross(x1, y1, x2, y2, 0, 0, maxX, 0) : 
+			MathHelper::getLinesCross(x1, y1, x2, y2, 0, maxY, maxX, maxY);
+	}
+}
+
+
+Vec2Double getShootingCrossBorderPoint(const Vec2Double& position, double lastAngle, double maxX, double maxY) {
+	
+	if (abs(lastAngle) < TOLERACNE) {
+		return Vec2Double(maxX, position.y);
+	}
+
+	if (abs(lastAngle - M_PI) < TOLERACNE) {
+		return Vec2Double(0, position.y);
+	}
+
+	if (abs(lastAngle - M_PI/2) < TOLERACNE) {
+		return Vec2Double(position.x, 0);
+	}
+
+	if (abs(lastAngle + M_PI / 2) < TOLERACNE) {
+		return Vec2Double(position.x, maxY);
+	}
+	
+	const auto x1 = position.x;
+	const auto y1 = position.y;
+	const auto x2 = position.x + cos(lastAngle);
+	const auto y2 = position.y + sin(lastAngle);
+
+	if (abs(lastAngle) < M_PI/2) {
+
+		const auto vertCross = MathHelper::getLinesCross(x1, y1, x2, y2, maxX, 0, maxX, maxY);
+		if (vertCross.y >= 0 && vertCross.y < maxY) {
+			return vertCross;
+		}
+
+		return lastAngle > 0 ? 
+			MathHelper::getLinesCross(x1, y1, x2, y2, 0, maxY, maxX, maxY) : 
+			MathHelper::getLinesCross(x1, y1, x2, y2, 0, 0, maxX, 0);
+	}
+	else {
+		const auto vertCross = MathHelper::getLinesCross(x1, y1, x2, y2, 0, 0, 0, maxY);
+		if (vertCross.y >= 0 && vertCross.y < maxY) {
+			return vertCross;
+		}
+
+		return lastAngle > 0 ? 
+			MathHelper::getLinesCross(x1, y1, x2, y2, 0, maxY, maxX, maxY) : 
+			MathHelper::getLinesCross(x1, y1, x2, y2, 0, 0, maxX, 0);
 	}
 }
 
 void drawBullets(Debug& debug, const Game& game) {
+	
 	const auto maxX = game.level.tiles.size() * TILE_SIZE;
 	const auto maxY = game.level.tiles[0].size() * TILE_SIZE;
 	for (const auto& bullet : game.bullets) {		
@@ -57,14 +112,18 @@ void drawBullets(Debug& debug, const Game& game) {
 		const auto bulletTiles = MathHelper::getLineSquares(bullet.position, crossPoint, 1);
 		const pair<int, int> *firstWallTile = nullptr;
 		for (const auto& bt : bulletTiles) {
+			if (bt.first == 0 || bt.second == 0 || bt.first == game.level.tiles.size() - 1 || bt.second == game.level.tiles[0].size() - 1) {
+				break; //игнор крайних стен
+			}
+
 			if (game.level.tiles[bt.first][bt.second] == Tile::WALL) {
 				firstWallTile = &bt;
 				break;
 			}
 		}
 
-		if (firstWallTile != nullptr) {
-			crossPoint = Vec2Double(firstWallTile->first + TILE_SIZE/2, firstWallTile->second + TILE_SIZE/2);
+		if (firstWallTile != nullptr) { //TODO: брать нормальное пересечение со стеной
+			crossPoint = Vec2Double(firstWallTile->first + 0.5, firstWallTile->second + 0.5);
 		}
 
 		const auto debugBullet = vec2DoubleToVec2Float(bullet.position);
@@ -73,8 +132,45 @@ void drawBullets(Debug& debug, const Game& game) {
 	}
 }
 
+void drawPossibleShoot(Debug& debug, const Unit& unit, const Game& game) {
+	if (unit.weapon == nullptr || (*unit.weapon).lastAngle == nullptr) return;
+	
+	const auto maxX = game.level.tiles.size() * TILE_SIZE;
+	const auto maxY = game.level.tiles[0].size() * TILE_SIZE;
+
+
+	const auto weaponPoistion = Vec2Double(unit.position.x, unit.position.y + unit.size.y / 2);
+
+	auto crossPoint = getShootingCrossBorderPoint(
+		weaponPoistion,
+		*(*unit.weapon).lastAngle, maxX, maxY);
+
+	const auto bulletTiles = MathHelper::getLineSquares(weaponPoistion, crossPoint, 1);
+	const pair<int, int> *firstWallTile = nullptr;
+	for (const auto& bt : bulletTiles) {
+		if (bt.first == 0 || bt.second == 0 || bt.first == game.level.tiles.size() - 1 || bt.second == game.level.tiles[0].size() - 1) {
+			break; //игнор крайних стен
+		}
+
+		if (game.level.tiles[bt.first][bt.second] == Tile::WALL) {
+			firstWallTile = &bt;
+			break;
+		}
+	}
+
+	if (firstWallTile != nullptr) { //TODO: брать нормальное пересечение со стеной
+		crossPoint = Vec2Double(firstWallTile->first + 0.5, firstWallTile->second + 0.5);
+	}
+
+	const auto debugUnit = vec2DoubleToVec2Float(weaponPoistion);
+	const auto debugCrossPoint = vec2DoubleToVec2Float(crossPoint);
+	debug.draw(CustomData::Line(debugUnit, debugCrossPoint, 0.1, ColorFloat(0, 0, 255, 1)));
+
+}
+
 bool isVisibleEnemy(const Unit& me, const Unit& enemy, const Game& game) {
-	auto squares = MathHelper::getLineSquares(me.position, enemy.position, 1);
+	const auto weaponPoistion = Vec2Double(me.position.x, me.position.y + me.size.y / 2);
+	auto squares = MathHelper::getLineSquares(weaponPoistion, enemy.position, 1);
 	const auto wall = find_if(squares.begin(), squares.end(), [game](const auto& p) {return game.level.tiles[p.first][p.second] == Tile::WALL; });
 	return wall == squares.end();
 }
@@ -111,6 +207,7 @@ UnitAction MyStrategy::getAction(const Unit &unit, const Game &game,
       std::string("Target pos: ") + targetPos.toString()));
 
   drawBullets(debug, game);
+  drawPossibleShoot(debug, unit, game);
 
   //debug.draw(CustomData::Line(Vec2Float(10, 10), Vec2Float(500, 500), 10, ColorFloat(255, 0, 0, 1)));
   Vec2Double aim = Vec2Double(0, 0);
