d86d0befce8e6c1a88d08dcc023db2afb1419d51
morozec <andre15@inbox.ru>
2019-12-27 22:27:20 +0400

    FIX установки мин - скорость = 0

--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -869,6 +869,7 @@ void setShootingAction(
 
 		if (enemyKilledCount > 0 && meLeftCount >= enemyLeftCount)
 		{
+			action.velocity = 0;
 			action.plantMine = true;
 			action.aim = { 0, -1 };
 			action.shoot = false;
