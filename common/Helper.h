93bc2440e9f27a19b772fb431a3dbd48d3c7eb1b
MorozovAP <andre15@inbox.ru>
2019-12-27 14:16:00 +0400

    увеличил SAFE_ATTACK_DIST до 7

--- common/Helper.h
+++ common/Helper.h
@@ -24,7 +24,7 @@ const auto MIN_SET_MINE_DIST = 7.0;
 
 const auto SAFE_OTHER_UNIT_PLANT_MINE_DIST = 5.0;
 
-const auto SAFE_ATTACK_DIST = 5.0;
+const auto SAFE_ATTACK_DIST = 7.0;
 const auto SAFE_DIST_TO_BORDER = 5.0;
 
 Vec2Float vec2DoubleToVec2Float(const Vec2Double& vec2Double);
